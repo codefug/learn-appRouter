680d94845b92e432f442ad99279424507247b87e
codefug <86544979+codefug@users.noreply.github.com>
2024-06-21 09:08:14 +0900

    deploy

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
   title: "HOME",
 };
 
-export async function getMovies() {
+async function getMovies() {
   const res = await fetch(API_URL);
   const json = await res.json();
   return json;
